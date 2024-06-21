295ca612577eb0b9b6d9ddad3cc76e81a17d8545
salimboughanmi <boughanmi1salim@gmail.com>
2024-06-21 05:02:29 +0100

    test technique

--- test_mosofty/lib/pages/employee_list_page.dart
+++ test_mosofty/lib/pages/employee_list_page.dart
@@ -1,40 +1,3 @@
-/* import 'package:flutter/material.dart';
-import 'package:test_mosofty/models/employee.dart';
-import 'package:test_mosofty/services/api_service.dart';
-
-class EmployeeListPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Liste des employés')),
-      body: FutureBuilder<List<Employee>>(
-        future: ApiService.getEmployees(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Erreur: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text('Aucun employé trouvé'));
-          } else {
-            final employees = snapshot.data!;
-            return ListView.builder(
-              itemCount: employees.length,
-              itemBuilder: (context, index) {
-                final employee = employees[index];
-                return ListTile(
-                  title: Text('${employee.nom} ${employee.prenom}'),
-                  subtitle: Text(employee.mail),
-                );
-              },
-            );
-          }
-        },
-      ),
-    );
-  }
-}
- */
 import 'package:flutter/material.dart';
 import 'package:test_mosofty/models/employee.dart';
 import 'package:test_mosofty/services/api_service.dart';
@@ -63,7 +26,6 @@ class EmployeeListPage extends StatelessWidget {
                   title: Text('${employee.nom} ${employee.prenom}'),
                   subtitle: Text(employee.mail),
                   onTap: () {
-                    // Naviguer vers la page de mise à jour avec l'ID de l'employé
                     Navigator.pushNamed(context, '/update_employee',
                         arguments: employee.id);
                   },
